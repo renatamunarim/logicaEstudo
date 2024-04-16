1919047d71e4a88cc0bcb508815edbec8cc565d2
renatamunarim <renatamunarim@hotmail.com>
2024-04-15 21:42:55 -0300

    Atualizando lista de exercicio

--- Exercicio de Repetição/script.js
+++ Exercicio de Repetição/script.js
@@ -1,2 +1,37 @@
+let resultado = document.getElementById("divResultado")
 //Parte 1
-//1. Faca um programa que calcule o quadrado de cada numero de 1 a 10.
+// 3.Calcule a soma dos números de 1 a 100.
+// let contador = 1
+// let soma = 0
+// while(contador<=100){
+//     soma += contador
+//     contador ++
+// } 
+// // resultado.innerHTML = "Resultado da soma: " + soma
+// //4.Imprima a tabuada do 5.
+// let contador = 1
+// while(contador <=10){
+//     resultado.innerHTML += `5 x ${contador} = ${5* contador}<br>`
+//     contador++
+// }
+//6. Imprima os números ímpares de 1 a 20.
+// let contador = 1
+// while(contador <=20){
+//     if(contador % 2 == 1){
+//         resultado.innerHTML += `${contador} `
+//     }
+//     contador++
+// }
+// //7. Imprima os números de 1 a 10 em ordem crescente, depois imprima-os em ordem decrescente.
+// let contador = 1
+// while(contador < 10){
+//     resultado.innerHTML += `${contador} `
+//     contador++
+// }
+// while(contador > 1){
+//     resultado.innerHTML += `${contador} `
+//     contador--
+// }
+//12. Dada uma lista de números, encontre o maior e o menor valor.
+let contador
+let numero = Number(prompt(`D`))
